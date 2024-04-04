9dca15d3482d604cd50e758ba8e3a66eac659105
Martyn van Dijke <martijnvdijke600@gmail.com>
2024-04-04 22:06:43 +0200

    added migrations

--- server/ledit/admin.py
+++ server/ledit/admin.py
@@ -1,5 +1,5 @@
 # admin.py
 from django.contrib import admin
-from .models import SiteSettings
+from .models import LEDitSettings
 
-admin.site.register(SiteSettings)
+admin.site.register(LEDitSettings)
--- server/ledit/models.py
+++ server/ledit/models.py
@@ -1,13 +1,6 @@
 from django.db import models
 
 
-class SiteSettings(models.Model):
-    site_name = models.CharField(max_length=100)
-    logo = models.ImageField(upload_to="logos/")
-    email = models.EmailField()
-    # Add more fields as needed
-
-
 class LEDitSettings(models.Model):
     timeout = models.FloatField(max_length=10)
     sonarr = models.BooleanField(default=False)
