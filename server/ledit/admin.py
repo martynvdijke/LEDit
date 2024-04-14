60f2f0d99c29dfda5501b8ba367cdfab01942a16
Martyn van Dijke <martijnvdijke600@gmail.com>
2024-04-14 17:24:18 +0200

    feat: started on models

--- server/ledit/admin.py
+++ server/ledit/admin.py
@@ -1,5 +1,6 @@
 # admin.py
 from django.contrib import admin
-from .models import LEDitSettings
+from .models import GeneralSettings, Image
 
-admin.site.register(LEDitSettings)
+admin.site.register(GeneralSettings)
+admin.site.register(Image)
--- server/ledit/models.py
+++ server/ledit/models.py
@@ -1,7 +1,41 @@
 from django.db import models
 
+class Image(models.Model):
+    image = models.ImageField(upload_to="custom_images")
 
-class LEDitSettings(models.Model):
+class DeviceSettings(models.Model):
+    ip = models.GenericIPAddressField()
+    port = models.IntegerField(default="6270")
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    width = models.IntegerField(default="64")
+    height = models.IntegerField(default="64")
+
+class SonarrSettings(models.Model):
+    token = models.CharField(max_length=100)
+    url = models.CharField(max_length=100)
+
+class RadarrSettings(models.Model):
+    token = models.CharField(max_length=100)
+    url = models.CharField(max_length=100)
+
+class F1Settings(models.Model):
+    pass
+
+class WeatherSettings(models.Model):
+    pass
+
+class HomeAssistantSettings(models.Model):
+    token = models.CharField(max_length=100)
+    url = models.CharField(max_length=100)
+
+class UntappedSettings(models.Model):
+    pass
+
+class StocksTrackerSettings(models.Model):
+    pass
+
+class GeneralSettings(models.Model):
     timeout = models.FloatField(max_length=10)
     sonarr = models.BooleanField(default=False)
     radarr = models.BooleanField(default=False)
@@ -11,3 +45,4 @@ class LEDitSettings(models.Model):
     untapped = models.BooleanField(default=False)
     stocks_tracker = models.BooleanField(default=False)
     crypto_tracker = models.BooleanField(default=False)
+    images = models.ManyToManyField(Image)
