236430fe3421d9792b72707d1d2f40e8caafe1ae
Martyn van Dijke <martijnvdijke600@gmail.com>
2024-04-29 21:23:50 +0200

    p5 test

--- server/ledit/render_model.py
+++ server/ledit/render_model.py
@@ -3,6 +3,7 @@ from PIL import Image, ImageDraw, ImageFont
 from io import BytesIO
 import base64
 import os
+import p5
 
 class RenderModel(models.Model):
 
@@ -20,12 +21,32 @@ class RenderModel(models.Model):
 
         return png_data.getvalue()
 
+    def setup(self):
+        p5.size(128,64)
+        font = p5.create_font("PixelifySans.ttf", 32)
+        p5.text_font(font)
+        p5.no_loop()
+        p5.fill(0)
+        p5.background(204)
+
+    def draw(self):
+        p5.background(204)
+        p5.text("LAX", (0, 40))
+        p5.text("LHR", (0, 70))
+        p5.text("TXL", (0, 100))
+    
+        p5.save_canvas("test.png")
+    
+
     def get_png(self):
         width = 640
         height = 640
         image = Image.new("RGB", (width, height), "white")
         draw = ImageDraw.Draw(image)
         print(os.getcwd())
+        p5.run()
+        self.draw()
+
 
         draw.rectangle((50, 50, 150, 150), fill="red")
         font = ImageFont.truetype("PixelifySans.ttf")
